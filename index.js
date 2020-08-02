9afaf4e4cc97837635f8322322f166cc6d478bc3
Nihalroshan <nihalroshanpc@gmail.com>
2020-08-02 15:34:00 +0530

    chans

---
+++ index.js
@@ -0,0 +1,9 @@
+
+document.getElementById("sign").addEventListener("click",function(){
+    document.querySelector(".popup").style.display = "flex";
+})
+    
+
+document.getElementById("close").addEventListener("click",function(){
+    document.querySelector(".popup").style.display = "none"
+})
