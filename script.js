8471c2f4719e44cdc4d3a8eef7644b87afb680a7
Nihalroshan <nihalroshanpc@gmail.com>
2020-08-03 11:43:27 +0530

    added greet

--- script.js
+++ script.js
@@ -2,8 +2,28 @@
 document.getElementById("sign").addEventListener("click",function(){
     document.querySelector(".popup").style.display = "flex";
 })
-    
+
+function close(){
+    document.querySelector(".popup").style.display = "none";
+
+}    
 
 document.getElementById("close").addEventListener("click",function(){
-    document.querySelector(".popup").style.display = "none"
+    close();
+})
+
+
+document.getElementById("login").addEventListener("click",function(){
+    
+    var name = document.getElementById("username").value;
+    var h1 =  document.createElement("h1");
+    var node = document.createTextNode("Hi "+name +" , Welcome.");
+    h1.appendChild(node);
+    var element = document.getElementById("greet");
+    element.appendChild(h1);
+
+    
+    close();
+    
+
 })
